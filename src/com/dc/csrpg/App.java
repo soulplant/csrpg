319343e17317852d22f247b7f9cfb34e0d8f6ed2
James Kozianski <soulplant@gmail.com>
2012-08-25 10:32:40 +1000

    Make both panels appear side by side.

--- src/com/dc/csrpg/App.java
+++ src/com/dc/csrpg/App.java
@@ -1,6 +1,9 @@
 package com.dc.csrpg;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class App {
   public static void main(String[] args) {
@@ -10,7 +13,10 @@ public class App {
     TownModel model = new TownModel();
     TownPanel townPanel = new TownPanel(new TownController(model), model);
 
-    frame.add(townPanel);
+    JPanel panel = new JPanel();
+    panel.add(townPanel, BorderLayout.CENTER);
+    panel.add(new BattlePanel(), BorderLayout.LINE_END);
+    frame.add(panel);
     frame.pack();
     frame.setVisible(true);
   }
