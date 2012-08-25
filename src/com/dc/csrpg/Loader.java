8f1a232bfa3cdd63e0887de94a3e70e79d2102fd
James Kozianski <soulplant@gmail.com>
2012-08-25 14:44:23 +1000

    Generalise grid loader.

--- src/com/dc/csrpg/Loader.java
+++ src/com/dc/csrpg/Loader.java
@@ -1,15 +1,37 @@
 package com.dc.csrpg;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
 
 public class Loader {
+  private static class EntityDescriptor {
+    public enum Type {
+      TILE
+    }
+    public Type type;
+    public Tile.Type tileType;
+    public boolean passable;
+
+    public Tile toTile() {
+      if (type != Type.TILE) {
+        throw new IllegalStateException(type + " != TILE");
+      }
+      return new Tile(tileType, passable);
+    }
+  }
+
   public static Grid<Tile> loadGridFromFile(String filename) {
     List<String> lines = Util.readFile(filename);
     if (lines.isEmpty()) {
       throw new IllegalArgumentException();
     }
     int worldWidth = lines.get(0).length();
-    int worldHeight = lines.size();
+    int emptyLineIndex = lines.indexOf("");
+    int worldHeight = emptyLineIndex < 0 ? lines.size() : emptyLineIndex;
+
     if (worldHeight % Constants.SCREEN_HEIGHT_TILES != 0) {
       throw new IllegalArgumentException("height of " + worldHeight + " should be divisible by " + Constants.SCREEN_HEIGHT_TILES);
     }
@@ -21,23 +43,36 @@ public class Loader {
       }
     };
 
-    for (int y = 0; y < lines.size(); y++) {
+    Map<Character, EntityDescriptor> entityMap =
+        new HashMap<Character, EntityDescriptor>();
+
+    // Parse the entities.
+    for (int y = worldHeight + 1; y < lines.size(); y++) {
+      String line = lines.get(y);
+      char entityChar = line.charAt(0);
+      Gson gson = new Gson();
+      EntityDescriptor entityDescriptor = gson.fromJson(line.substring(1),
+          EntityDescriptor.class);
+      entityMap.put(entityChar, entityDescriptor);
+    }
+
+    // Parse the entity map.
+    for (int y = 0; y < worldHeight; y++) {
       String line = lines.get(y);
       if (worldWidth % Constants.SCREEN_WIDTH_TILES != 0) {
         throw new IllegalArgumentException("width of " + worldWidth + " should be divisible by " + Constants.SCREEN_WIDTH_TILES);
       }
       for (int x = 0; x < line.length(); x++) {
-        grid.set(x, y, charToTile(line.charAt(x)));
+        grid.set(x, y, makeTile(entityMap.get(line.charAt(x))));
       }
     }
     return grid;
   }
 
-  private static Tile charToTile(char c) {
-    switch (c) {
-      case '.': return new Tile(Tile.Type.GRASS, true);
-      case 'w': return new Tile(Tile.Type.WALL, false);
-      default: throw new IllegalArgumentException();
+  private static Tile makeTile(EntityDescriptor entityDescriptor) {
+    if (entityDescriptor.type != EntityDescriptor.Type.TILE) {
+      return new Tile(Tile.Type.GRASS, true);
     }
+    return entityDescriptor.toTile();
   }
 }
